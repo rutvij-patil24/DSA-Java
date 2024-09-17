ab7558f9e35922b717703ee13dc6a145d6fa41f5
Rutvij Patil <66994155+rutvij-patil24@users.noreply.github.com>
2024-09-17 18:13:45 -0400

    Minor Changes

---
+++ Java Basics/hello.java
@@ -0,0 +1,18 @@
+class hello{
+    public static void main(String[] args) {
+        System.out.println("Hello World");
+        
+        // Primitive datatypes
+        int num = 5;
+        double d = 6.9;
+        float f = 4.5f;
+        long l = 45678;
+        char c = 'R';
+
+        System.out.println(num + d);
+        System.out.println(num + " " + d);
+
+        
+
+    }
+}
